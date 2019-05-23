0b8245bfa3ffc44cfa191dfe87ca71b0ac7ecee2
Salamkams <salamsurfing@gmail.com>
2019-05-23 18:00:31 +0400

    Modification of pages controller, home view, creation of team and contact view, add of projects controller, modification of the routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,14 @@
 class PagesController < ApplicationController
   def home
+    @today = Date.today + 1
+    @projects = ["Expo2020", "JBR", "JLT", "Marina"]
   end
+
+  def team
+    @members = ["Karim", "Musab", "Hassan"]
+  end
+  def join_us
+    render plain: "Send us a mail"
+  end
+
 end
---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,7 @@
+class ProjectsController < ApplicationController
+
+  def index
+    @projects = ["Expo2020", "JBR", "JLT", "Marina"]
+  end
+
+end
