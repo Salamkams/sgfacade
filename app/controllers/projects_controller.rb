c436398f9f377d421046d148ea07c5ef72ec565b
Salamkams <salamsurfing@gmail.com>
2019-05-24 15:58:36 +0400

    Add model Project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,21 +1,11 @@
 class ProjectsController < ApplicationController
 
-  PROJECTS =[
-            {name: "Expo2020", stage: "design"},
-            {name: "JBR", stage: "construction"},
-            {name: "JLT", stage: "construction"},
-            {name: "Atlantis", stage: "finished"}]
-
   def index
-    if params[:filter]
-      @projects = PROJECTS.select {|project| project[:stage] == params[:filter]}
-    else
-     @projects =PROJECTS
-    end
+      @projects = Project.all
   end
 
   def show
-    @project = PROJECTS[params[:id].to_i]
+    @project = Project.find(params[:id])
   end
 
 end
