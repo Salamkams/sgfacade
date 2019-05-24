68bc0cfb2ab037863e7eac5fd2671163821e1988
Salamkams <salamsurfing@gmail.com>
2019-05-25 01:48:23 +0400

    Add update and destroy actions +refacto

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,11 +1,12 @@
 class ProjectsController < ApplicationController
+  before_action :find_project, only:[:show, :edit, :update, :destroy]
+
 
   def index
-      @projects = Project.all
+    @projects = Project.all
   end
 
   def show
-    @project = Project.find(params[:id])
   end
 
   def new
@@ -25,15 +26,26 @@ class ProjectsController < ApplicationController
   end
 
   def update
+    if @project.update(project_params)
+      redirect_to projects_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @project.destroy
+    redirect_to projects_path
   end
 
-  private
 
+  private
   def project_params
     params.require(:project).permit(:name, :stage)
+  end
+
+  def find_project
+    @project = Project.find(params[:id])
   end
 
 end
