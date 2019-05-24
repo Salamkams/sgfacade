19993f21bbc47dc9fdcd08bcd13c3229688b8075
Salamkams <salamsurfing@gmail.com>
2019-05-24 16:37:36 +0400

    Add resource routing and CRUD actions for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -8,4 +8,19 @@ class ProjectsController < ApplicationController
     @project = Project.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,25 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
-  get "/team" => "pages#team"
-  get "/contact" => "pages#join_us"
-  get "projects" => "projects#index"
-  get "projects/:id" => "projects#show"
+  resources :projects
+
+  # get "/team" => "pages#team"
+  # get "/contact" => "pages#join_us"
+
+  # # Read action routes
+  # get "projects" => "projects#index"
+  # get "projects/:id" => "projects#show"
+
+  # # Create action routes
+  # get "/projects/new" => "projects#new"
+  # post "/projects" => "projects#create"
+
+  # # Update action routes
+  # get "/projects/:id/edit" => "projects#edit"
+  # patch "/projects/:id" => "projects#update"
+
+  # # Delete action routes
+  # delete "/projects/:id" => "projects#destroy"
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
